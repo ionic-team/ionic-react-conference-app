0b4327423c70381c72b441cbb32fdf4f946a1f8e
Josh Thomas <jthoms1@gmail.com>
2019-01-02 15:18:28 -0600

    Update to use TabNav for navigation

--- src/pages/AppStack.tsx
+++ src/pages/AppStack.tsx
@@ -3,11 +3,11 @@ import SchedulePage from './SchedulePage';
 import SessionDetail from './SessionDetail';
 import SpeakerList from './SpeakerList';
 import SpeakerDetail from './SpeakerDetail';
-import Map from './Map';
+import MapView from './Map';
 import About from './About';
-import StackNav from '../components/StackNav';
-import { IonTabs, IonTab, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
-import { withRouter, RouteComponentProps, matchPath } from 'react-router';
+import StackNav from '../navigation/StackNav';
+import TabNav from '../navigation/TabNav';
+import { IonTab, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/react';
 
 const ScheduleStack: React.SFC<any> = (props) => (
   <StackNav
@@ -52,119 +52,42 @@ const SpeakerStack: React.SFC<any> = (props) => (
   ></StackNav>
 );
 
-const MapStack: React.SFC<any> = (props) => (
-  <StackNav
-    {...props}
-    navViews={[
-      {
-        name: 'map',
-        title: 'Map',
-        path: '',
-        getView: () => (Map)
-      }
-    ]}
-  ></StackNav>
+const AppStack: React.SFC = () => (
+  <div className="ion-page">
+    <TabNav>
+      <IonTab tab="schedule">
+        <SchedulePage/>
+      </IonTab>
+      <IonTab tab="speakers">
+        <SpeakerList/>
+      </IonTab>
+      <IonTab tab="map">
+        <MapView/>
+      </IonTab>
+      <IonTab tab="about">
+        <About/>
+      </IonTab>
+
+      <IonTabBar slot="bottom">
+        <IonTabButton tab="schedule">
+          <IonIcon name="calendar" />
+          <IonLabel>Schedule</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="speakers">
+          <IonIcon name="contacts" />
+          <IonLabel>Speakers</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="map">
+          <IonIcon name="map" />
+          <IonLabel>Map</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="about">
+          <IonIcon name="information-circle" />
+          <IonLabel>About</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </TabNav>
+  </div>
 );
 
-const AboutStack: React.SFC<any> = (props) => (
-  <StackNav
-    {...props}
-    navViews={[
-      {
-        name: 'about',
-        title: 'About',
-        path: '',
-        getView: () => (About)
-      }
-    ]}
-  />
-);
-
-
-class AppStack extends Component<RouteComponentProps> {
-  constructor(props: RouteComponentProps) {
-    super(props);
-  }
-
-  urlMatchHandler = (path: string) => {
-    return matchPath(this.props.location.pathname, { path: path });
-  }
-
-  onPageChange = (basePath: string, subPath: string, params: any) => {
-    var resolvedSubPath = subPath;
-
-    Object.keys(params).forEach((name) => {
-      const value = params[name];
-      resolvedSubPath = resolvedSubPath.replace(`:${name}`, value);
-    });
-
-    const activePath = `${basePath}/${resolvedSubPath}`;
-    this.props.history.replace(activePath);
-  }
-
-  render() {
-    return (
-      <div className="ion-page">
-        <IonTabs>
-          <IonTab tab="schedule">
-            <SchedulePage/>
-           {/**
-            <ScheduleStack
-              onPageChange={this.onPageChange}
-              urlMatchHandler={this.urlMatchHandler}
-            />
-           */}
-          </IonTab>
-          <IonTab tab="speakers">
-            <SpeakerList/>
-           {/**
-            <SpeakerStack
-              onPageChange={this.onPageChange}
-              urlMatchHandler={this.urlMatchHandler}
-            />
-           */}
-          </IonTab>
-          <IonTab tab="map">
-            <Map/>
-           {/**
-            <MapStack
-              onPageChange={this.onPageChange}
-              urlMatchHandler={this.urlMatchHandler}
-            />
-           */}
-          </IonTab>
-          <IonTab tab="about">
-            <About/>
-           {/**
-            <AboutStack
-              onPageChange={this.onPageChange}
-              urlMatchHandler={this.urlMatchHandler}
-            />
-           */}
-          </IonTab>
-
-          <IonTabBar slot="bottom">
-            <IonTabButton tab="schedule">
-              <IonIcon name="calendar" />
-              <IonLabel>Schedule</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="speakers">
-              <IonIcon name="contacts" />
-              <IonLabel>Speakers</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="map">
-              <IonIcon name="map" />
-              <IonLabel>Map</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="about">
-              <IonIcon name="information-circle" />
-              <IonLabel>About</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </div>
-    );
-  }
-}
-
-export default withRouter(AppStack);
+export default AppStack;
