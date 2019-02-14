cd9ccbe109bfa20bda29bb535567eeecef8af689
Daniel Sogl <mytechde@outlook.com>
2019-02-14 07:44:00 +0100

    refactor(app): use hooks for Signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,59 +1,67 @@
-import React, { Component } from 'react';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonList, IonItem, IonLabel, IonInput, IonButton } from '@ionic/react';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonMenuButton,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React, { FunctionComponent, useRef, useState } from 'react';
 
-type State = {
-  username: string | null,
-  password: string | null
+interface State {
+  username: string | null;
+  password: string | null;
 }
 
-export default class Signup extends Component<{},State> {
-  signupFormRef: React.Ref<HTMLFormElement>
+const Signup: FunctionComponent = () => {
+  const signupFormRef = useRef<HTMLFormElement>(null);
+  const [state, setState] = useState({
+    username: null,
+    password: null
+  });
 
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      username: null,
-      password: null
-    }
-    this.signupFormRef = React.createRef();
-  }
+  const onSignup = () => {};
 
-  onSignup() {}
-  render() {
-    return (
-      <>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Signup</IonTitle>
-          </IonToolbar>
-        </IonHeader>,
-
-        <IonContent class="page-user">
-          <div className="logo">
-            <img src="/assets/img/appicon.svg" alt="Ionic Logo"/>
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Signup</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      ,
+      <IonContent class="page-user">
+        <div className="logo">
+          <img src="/assets/img/appicon.svg" alt="Ionic Logo" />
+        </div>
+        <form ref={signupFormRef}>
+          <IonList no-lines>
+            <IonItem>
+              <IonLabel color="primary">Username</IonLabel>
+              <IonInput value={state.username} name="username" type="text" required />
+            </IonItem>
+            <IonItem>
+              <IonLabel color="primary">Password</IonLabel>
+              <IonInput value={state.password} name="password" type="password" required />
+            </IonItem>
+          </IonList>
+          <div>
+            <IonButton onClick={() => onSignup()} type="submit">
+              Create
+            </IonButton>
           </div>
-          <form ref={this.signupFormRef}>
-            <IonList no-lines>
-              <IonItem>
-                <IonLabel color="primary">Username</IonLabel>
-                <IonInput value={this.state.username} name="username" type="text" required>
-                </IonInput>
-              </IonItem>
-              <IonItem>
-                <IonLabel color="primary">Password</IonLabel>
-                <IonInput value={this.state.password} name="password" type="password" required>
-                </IonInput>
-              </IonItem>
-            </IonList>
-            <div>
-              <IonButton onClick={() => this.onSignup()} type="submit">Create</IonButton>
-            </div>
-          </form>
-        </IonContent>
-      </>
-    );
-  }
-}
+        </form>
+      </IonContent>
+    </>
+  );
+};
+
+export default Signup;
