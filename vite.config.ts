41abe8c26edcddba8bb27df618f22563476b6f2f
ShaneK <shane@shanessite.net>
2025-03-05 11:01:35 -0800

    feat(vite): upgrading vite and sass to get rid of deprecated packages warning

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +8,14 @@ export default defineConfig({
     port: 3000
   },
   define: {
-    'process.env': process.env
-  }
+    'process.env': process.env,
+    __HMR_CONFIG_NAME__: JSON.stringify('vite')
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern',
+      },
+    },
+  },
 })
