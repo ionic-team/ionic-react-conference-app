cd4eff37df4a529a5b8fd9b665461f1350d797cb
Brandy Smith <brandyscarney@users.noreply.github.com>
2026-09-21 10:05:21 -0400

    chore(deps): remove stale swiper dependency (#169)
    Co-authored-by: Brandy Smith <6577830+brandyscarney@users.noreply.github.com>

--- src/pages/Tutorial.tsx
+++ src/pages/Tutorial.tsx
@@ -62,7 +62,7 @@ const Tutorial: React.FC<TutorialProps> = ({
       <IonContent fullscreen>
         <div className="slider" ref={sliderRef}>
           <section>
-            <div className="swiper-item">
+            <div className="slide-content">
               <img
                 src="assets/img/ica-slidebox-img-1.png"
                 alt=""
@@ -79,7 +79,7 @@ const Tutorial: React.FC<TutorialProps> = ({
             </div>
           </section>
           <section>
-            <div className="swiper-item">
+            <div className="slide-content">
               <img
                 src="assets/img/ica-slidebox-img-2.png"
                 alt=""
@@ -94,33 +94,29 @@ const Tutorial: React.FC<TutorialProps> = ({
             </div>
           </section>
           <section>
-            <div className="swiper-item">
-              <img
-                src="assets/img/ica-slidebox-img-3.png"
-                alt=""
-                className="slide-image"
-              />
-              <h2 className="slide-title">What is Ionic Appflow?</h2>
-              <p>
-                <b>Ionic Appflow</b> is a powerful set of services and features
-                built on top of Ionic Framework that brings a totally new level
-                of app development agility to mobile dev teams.
-              </p>
-            </div>
+            <img
+              src="assets/img/ica-slidebox-img-3.png"
+              alt=""
+              className="slide-image"
+            />
+            <h2 className="slide-title">What is Ionic Appflow?</h2>
+            <p>
+              <b>Ionic Appflow</b> is a powerful set of services and features
+              built on top of Ionic Framework that brings a totally new level of
+              app development agility to mobile dev teams.
+            </p>
           </section>
           <section>
-            <div className="swiper-item">
-              <img
-                src="assets/img/ica-slidebox-img-4.png"
-                alt=""
-                className="slide-image"
-              />
-              <h2 className="slide-title">Ready to Play?</h2>
-              <IonButton fill="clear" onClick={startApp}>
-                Continue
-                <IonIcon slot="end" icon={arrowForward} />
-              </IonButton>
-            </div>
+            <img
+              src="assets/img/ica-slidebox-img-4.png"
+              alt=""
+              className="slide-image"
+            />
+            <h2 className="slide-title">Ready to Play?</h2>
+            <IonButton fill="clear" onClick={startApp}>
+              Continue
+              <IonIcon slot="end" icon={arrowForward} />
+            </IonButton>
           </section>
         </div>
       </IonContent>
